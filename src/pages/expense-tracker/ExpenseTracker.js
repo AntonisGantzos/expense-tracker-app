04e0bc3d24db40e3ff4125d0b4c08018b89d1274
AntonisGantzos <antonisgantzos@gmail.com>
2024-10-23 11:07:58 +0300

    -Add Expense tracker form

--- src/pages/expense-tracker/ExpenseTracker.js
+++ src/pages/expense-tracker/ExpenseTracker.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 function ExpenseTracker() {
   return (
-    <div>ExpenseTracker</div>
+    <div className='expense-tracker'>
+      <div>
+        <h1>Expense Tracker</h1>
+        <div className='balance'>
+          <h3>Your Balance</h3>
+          <h2>0.00$</h2>
+        </div>
+        <div className='summary'>
+          <div className='income'>
+            <h4>Income</h4>
+            <p>0.00$</p>
+          </div>
+          <div className='expenses'>
+            <h4>Expenses</h4>
+            <p>0.00$</p>
+          </div>
+        </div>
+        <form className='add-transaction'>
+          <input type='text' placeholder='Expense Description' required/>
+          <input type='number' placeholder='Amount' required/>
+          <input type='radio' id='expense' value='expense'/>
+          <label htmlFor='expense'>Expense</label>
+          <input type='radio' id='income' value='income'/>
+          <label htmlFor='income'>Income</label>
+          <button type='submit'>Add Transaction</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
