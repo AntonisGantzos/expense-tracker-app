6e609bfde5d71b6cb87f85c73886d3768d52582c
AntonisGantzos <antonisgantzos@gmail.com>
2024-10-23 10:49:09 +0300

    -Implement google authentication via a popup as an async function -keep information regarding the authentication result in an object -pass that object to localStorage in string form

--- src/pages/auth/Authentication.js
+++ src/pages/auth/Authentication.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { signInWithPopup } from 'firebase/auth'
 import {auth, provider} from '../../config/firebase-config'
+import { useNavigate } from 'react-router-dom'
 function Authentication() {
+  const navigate = useNavigate()
+
   const signInWithGoogle = async () => {
-     await signInWithPopup(auth, provider)
+     const results = await signInWithPopup(auth, provider)
+     console.log(results)
+     //store information relative to the authentication of the user in an object
+     const authInfo = {
+      userID : results.user.uid,
+      name : results.user.displayName,
+      profilePhoto: results.user.photoURL,
+      isAuth: true,
+     }
+     //pass this object as a string in localStorage
+     localStorage.setItem("auth", JSON.stringify(authInfo))
+     navigate("/expense-tracker")
   }
   return (
     <div className='login-page'>
       <p>Sign in with google to continue</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}></button>
+      <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   )
 }
